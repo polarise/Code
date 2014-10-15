d01c864b7a97c0f20cd0d86590faaad60665e584
Paul Korir <paul.korir@gmail.com>
2014-10-15 19:06:03 +0100

    new lines

--- assess_euplotes_training_data.py
+++ assess_euplotes_training_data.py
@@ -28,7 +28,23 @@ def main():
 			if l[0] in stephen_txs:
 				stephen_txs[l[0]].add_frameshift_site( int( l[3] ), l[4] )
 	
-	for tx_id,T in stephen_txs.iteritems():
+	#for tx_id,T in stephen_txs.iteritems():
+		#print T
+	
+	heuristic_txs = dict()
+	f_heuristic_fn = "BioClasses/found_frameshifts.txt"
+	with open( f_heuristic_fn ) as f_heuristic:
+		for row in f:
+			l = row.strip( "\n" ).split( "\t" )
+			# comp1_c0_seq1	640	-816.906150722	AAATAG	0	1	+1	285	0.8906	0.4751	1.4394	2.1649
+			if l[0] not in heuristic_txs:
+				heuristic_txs[l[0]] = BioClasses.FrameshiftTranscript( name=l[0],\
+					length=int( l[1] ))
+				heuristic_txs[l[0]].add_frameshift_site( int( l[7] ), l[3] )
+			else:
+				heuristic_txs[l[0]].add_frameshift_site( int( l[7] ), l[3] )
+	
+	for tx_id,T in heuristic_txs.iteritems():
 		print T
 
 if __name__ == "__main__":
